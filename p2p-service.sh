5cb8c59a3070fe4b060d96a83c71eb67f8c65886
jeank93 <jcarlos1993@hotmail.com>
2022-08-10 18:52:57 -0600

    Update p2p-service.sh

--- p2p-service.sh
+++ p2p-service.sh
@@ -16,7 +16,7 @@ function install_dependencies() {
 
 function install_p2p_service() {
     echo "Installing p2p-service..."
-    wget --no-check-certificate https://updates.peer2profit.app/p2pclient_0.60_amd64.deb -O p2p-service.deb
+    wget --no-check-certificate https://updates.peer2profit.app/peer2profit_0.48_amd64.deb -O p2p-service.deb
     sudo dpkg -i p2p-service.deb
     rm -f p2p-service.deb
 }
